cf9c18b0f89848f2c2b37084e877ae99b59ac628
fatma2090 <boudayafatima@gmail.com>
2024-04-19 01:30:19 +0200

    init

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 Welcome to Obliviate, a memory game based on the Harry Potter universe!
 
+![Obliviate](pictures/Game.jpg)
 
 ### Description:
 
-Obliviate is a memory game where players try to match pairs of Harry Potter character cards. It's a fun and challenging game suitable for all ages.
+Obliviate is a memory game where players try to match pairs of Harry Potter character cards. It's a fun and challenging game suitable for all ages and remember Muggles are not allowed !
 
 ### Features:
 
@@ -13,11 +14,12 @@ Obliviate is a memory game where players try to match pairs of Harry Potter char
     - Timer and score tracking
     - Restart functionality
     - Notification system for game events
-    - 
 
 ### How to Play:
 
-    - Say the magical spell "I solemnly swear I am up to no good" to unlock the secrets of Obliviate!
+![Harry Potter GIF](https://media1.tenor.com/m/84X3672KLCsAAAAC/harry-potter-harry-potter-characters.gif)
+
+    - Grab your wand and say the magical spell "I solemnly swear I am up to no good" to unlock the secrets of Obliviate!
     - Click the Start button to begin the game.
     - Click on cards to flip them and reveal the characters.
     - Try to match pairs of identical characters.
--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function startGame() {
 
 function flipCard() {
     if (!isGameStarted) {
-        return; // Do nothing if the game has not started
+        return; 
     }
     const cardId = this.getAttribute('data-id');
     cardsChosen.push(characters[cardId]);
@@ -77,21 +77,24 @@ function checkForMatch() {
     const cards = document.querySelectorAll('.card');
     const [optionOneId, optionTwoId] = cardsChosenId;
     if (cardsChosen[0].name === cardsChosen[1].name) {
-        showNotification('You found a match!');
         cards[optionOneId].classList.add('matched');
         cards[optionTwoId].classList.add('matched');
         cardsWon.push(cardsChosen);
     } else {
         cards[optionOneId].classList.remove('flip');
         cards[optionTwoId].classList.remove('flip');
-        showNotification('Sorry, try again!');
+        showNotification('Try again!', 'https://media1.tenor.com/m/njrw1mqiYu0AAAAC/smack-slapping.gif');
     }
     cardsChosen = [];
     cardsChosenId = [];
+
     if (cardsWon.length === characters.length / 2) {
-        showNotification('Congratulations! Mischief Managed!');
+        setTimeout(() => {
+            showNotification('Congratulations! Mischief Managed!', 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmMxajF1ajR3aGxwYXIxcW1ncm52Ym9nOWp2MnFyMmc5NWVwcWR0aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26gN16cJ6gy4LzZSw/giphy.gif');
+        }, 500); 
     }
 }
+
 
 document.getElementById('start-button').addEventListener('click', startGame);
 
@@ -99,35 +102,36 @@ createBoard();
 
 //////// Notification ///////
 
-function showNotification(message) {
+function showNotification(message, gifUrl) {
+
     const notification = document.getElementById('notification');
-    notification.textContent = message;
+
+    
+    const messageDiv = document.createElement('div');
+    messageDiv.textContent = message;
+    messageDiv.classList.add('notification-message');
+    
+    const gif = document.createElement('img');
+    gif.src = gifUrl;
+    gif.alt = 'Notification GIF';
+    gif.classList.add('notification-gif');
+    
+    notification.appendChild(messageDiv);
+    notification.appendChild(gif);
+
     notification.style.display = 'block';
+
     setTimeout(() => {
         notification.style.display = 'none';
-    }, 1500); 
+
+        notification.innerHTML = '';
+    }, 2000); 
 }
 
-//////// Score ///////
-
-let score = 0;
-
-function updateScore() {
-    document.getElementById("score").textContent = score;
-}
-function incrementScore() {
-    score++;
-    updateScore(); 
-}
-function resetScore() {
-    score = 0;
-    updateScore(); 
-}
-resetScore();
 
 ////// Start botton ///////
 
-let timer; 
+let timer;
 
 document.getElementById('start-button').addEventListener('click', () => {
     document.getElementById('start-button').style.display = 'none'; 
@@ -135,15 +139,17 @@ document.getElementById('start-button').addEventListener('click', () => {
 });
 
 function startTimer() {
-    let timeLeft = 30; 
+    let timeLeft = 30;
 
     timer = setInterval(() => {
         timeLeft--; 
         document.getElementById('timer').textContent = `Time Left: ${timeLeft} seconds`;
 
         if (timeLeft === 0) {
-            clearInterval(timer); 
-            alert('Time\'s up!'); 
+            clearInterval(timer);
+            if (cardsWon.length !== characters.length / 2) {
+                showNotification("Time's up!", "https://media1.tenor.com/m/K9BERPW2l2QAAAAC/vold-voldemort.gif");
+            }
         }
     }, 1000);
 }
@@ -151,14 +157,43 @@ function startTimer() {
 
 //////// Restart botton //////
 
-function restartGame() {
-    console.log("Restarting the game..."); 
-    location.reload();
-}
+class RestartButton {
+    constructor() {
+      this.button = document.getElementById('restart-button');
+      this.button.addEventListener('click', this.restartGame.bind(this));
+    }
+  
+    restartGame() {
+      console.log("Restarting the game..."); 
+      location.reload();
+    }
+  }
+  
+  const restartButton = new RestartButton();
+  
 
-document.getElementById("restart-button").addEventListener("click", restartGame);
+  ///////// AUDIO ////////
 
-
+  class MusicPlayer {
+    constructor() {
+      this.musicDiv = document.querySelector('.music');
+      this.audioElement = this.musicDiv.querySelector('audio');
+      this.playButton = this.musicDiv.querySelector('.play-button');
+      
+      this.playButton.addEventListener('click', this.togglePlay.bind(this));
+    }
+  
+    togglePlay() {
+      if (this.audioElement.paused) {
+        this.audioElement.play();
+      } else {
+        this.audioElement.pause();
+      }
+    }
+  }
+  
+  const musicPlayer = new MusicPlayer();
+  
 
 
 
